49e8cf042bdfd169839a864a103327765246f0cc
commit <1905269298@qq.com>
2024-12-22 18:39:04 +0800

    fix 编译错误

--- core/src/base/utils/string_converter.cpp
+++ core/src/base/utils/string_converter.cpp
@@ -8,9 +8,7 @@
 
 namespace yuan::base::encoding
 {
-    using namespace yuan::std;
-
-    std::string GBKToUTF8(const string &strGBK)
+    std::string GBKToUTF8(const std::string &strGBK)
     {
         int n = MultiByteToWideChar(CP_ACP, 0, strGBK.c_str(),  - 1, NULL, 0);
         auto *str1 = new WCHAR[n];
@@ -18,13 +16,13 @@ namespace yuan::base::encoding
         n = WideCharToMultiByte(CP_UTF8, 0, str1, -1, NULL, 0, NULL, NULL);
         const auto str2 = new char[n];
         WideCharToMultiByte(CP_UTF8, 0, str1,  - 1, str2, n, NULL, NULL);
-        string strOutUTF8 = str2;
+        std::string strOutUTF8 = str2;
         delete [] str1;
         delete [] str2;
         return strOutUTF8;
     }
 
-    std::string UTF8ToGBK(const string &strUTF8){
+    std::string UTF8ToGBK(const std::string &strUTF8){
         int len = MultiByteToWideChar(CP_UTF8, 0, strUTF8.c_str(), -1, NULL, 0);
         const auto wszGBK = new WCHAR[len + 1];
         memset(wszGBK, 0, (len+1)*sizeof(WCHAR));
@@ -34,7 +32,7 @@ namespace yuan::base::encoding
         memset(szGBK, 0, len + 1);
         WideCharToMultiByte(CP_ACP, 0, wszGBK,  - 1, szGBK, len, NULL, NULL);
         //strUTF8 = szGBK;
-        string strTemp(szGBK);
+        std::string strTemp(szGBK);
         delete [] szGBK;
         delete [] wszGBK;
         return strTemp;
